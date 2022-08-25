dd5d5982aa29346a4e25c8732492dede9d6ac25c
zerzkhane <mariamzerzkhane@gmail.com>
2022-08-25 09:57:49 +0200

    inscrii

--- inscrii.js
+++ inscrii.js
@@ -1,3 +1,3 @@
-var date=new Date();
-var DATE = date.getFullYear()+'/'+(date.getMonth()+1)+'/'+date.getDate();
-document.getElementById("DATE").innerText="Bonjour la date Aujourd'hui est : "+DATE;
+
+
+ 
